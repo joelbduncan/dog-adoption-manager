1d954e2d20bb706ad8741a09d8a9611dc59ad883
Joel Duncan <joel@slethen.io>
2019-02-20 21:22:06 +0000

    Add safety to SQL statements, add adoption status and mods to CRUD

--- addDog/ajaxupload.php
+++ addDog/ajaxupload.php
@@ -1,56 +1,57 @@
-<?php
+ <?php
 
-$valid_extensions = array('jpeg', 'jpg', 'png', 'gif', 'bmp'); // valid extensions
-$path = 'uploads/'; // upload directory
+//include database configuration file
+include('../config.php');
 
-if(!empty($_POST['name']) || !empty($_POST['email']) || $_FILES['image'])
-{
-	$img = $_FILES['image']['name'];
-	$tmp = $_FILES['image']['tmp_name'];
+//Create connection and select DB
+$db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 
-	// get uploaded file's extension
-	$ext = strtolower(pathinfo($img, PATHINFO_EXTENSION));
-
-	// can upload same image using rand function
-	$final_image = rand(1000,1000000).$img;
-
-	// check's valid format
-	if(in_array($ext, $valid_extensions)) 
-	{					
-		$path = $path.strtolower($final_image);	
-
-		if(move_uploaded_file($tmp,$path)) 
-		{
-			echo "<img src='$path' />";
-	$name = $_POST['name'];
-    $breed = $_POST['breed'];
-    $age = $_POST['age'];
-    $sex = $_POST['sex'];
-    $location = $_POST['location'];
-    $short_description = $_POST['short_description'];
-    $long_description = $_POST['long_description'];
-    $adopted = $_POST['adopted'];
-
-    //include database configuration file
-    include ('../config.php');
-
-	//Create connection and select DB
-	$db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
-
-	if($db->connect_error){
-	    die("Unable to connect database: " . $db->connect_error);
-	}
-
-    //insert form data in the database
-    $insert = $db->query("INSERT dogs (name,breed,age,sex,location,short_description,long_description,adopted,image) VALUES ('".$name."','".$breed."','".$age."','".$sex."','".$location."','".$short_description."','".$long_description."','".$adopted."','".$path."')");
-    //echo $insert?'ok':'err';
-		
-		}
-	} 
-	else 
-	{
-		echo 'invalid';
-	}
+if ($db->connect_error) {
+    die("Unable to connect database: " . $db->connect_error);
 }
 
-?>
+$valid_extensions = array(
+    'jpeg',
+    'jpg',
+    'png',
+    'gif',
+    'bmp'
+); // valid extensions
+$path             = 'uploads/'; // upload directory
+
+if (!empty($_POST['name']) || !empty($_POST['email']) || $_FILES['image']) {
+    $img = $_FILES['image']['name'];
+    $tmp = $_FILES['image']['tmp_name'];
+    
+    // get uploaded file's extension
+    $ext = strtolower(pathinfo($img, PATHINFO_EXTENSION));
+    
+    // can upload same image using rand function
+    $final_image = rand(1000, 1000000) . $img;
+    
+    // check's valid format
+    if (in_array($ext, $valid_extensions)) {
+        $path = $path . strtolower($final_image);
+        
+        if (move_uploaded_file($tmp, $path)) {
+            echo "<img src='$path' />";
+            $name              = mysqli_real_escape_string($db, $_POST['name']);
+            $breed             = mysqli_real_escape_string($db, $_POST['breed']);
+            $age               = mysqli_real_escape_string($db, $_POST['age']);
+            $sex               = mysqli_real_escape_string($db, $_POST['sex']);
+            $location          = mysqli_real_escape_string($db, $_POST['location']);
+            $short_description = mysqli_real_escape_string($db, $_POST['short_description']);
+            $long_description  = mysqli_real_escape_string($db, $_POST['long_description']);
+            $adopted           = mysqli_real_escape_string($db, $_POST['adopted']);
+            
+            //insert form data in the database
+            $insert = $db->query("INSERT dogs (name,breed,age,sex,location,short_description,long_description,adopted,image) VALUES ('" . $name . "','" . $breed . "','" . $age . "','" . $sex . "','" . $location . "','" . $short_description . "','" . $long_description . "','" . $adopted . "','" . $path . "')");
+            //echo $insert?'ok':'err';
+            
+        }
+    } else {
+        echo 'invalid';
+    }
+}
+
+?> 
